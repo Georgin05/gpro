d8a73add0110fe6ee765a5aae9120f03ea6ec0a6
Georgin05 <georginthomas05@gmail.com>
2025-07-01 16:43:27 +1100

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,30 @@
 <?php 
 $servername="localhost";
-$database="";
+$database="login";
 $username="root";
 $password="";
 
-$conn=new mysqli($servername,$database,$password);
+$conn=new mysqli($servername,$database,$database,$password);
 
 if($conn->connect_error){
-die("connection falied:")
+die("connection falied:".$conn->connect_error);
+}
+echo "Connected successfully";
 
+
+
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM login WHERE email='$email' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        echo "Login successful!";
+        // Redirect to a different page or perform other actions
+    } else {
+        echo "Invalid email or password.";
+    }
+}
+?>
