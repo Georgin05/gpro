e9742045c7535009ab0b8b334f22dd6556cfd89b
Georgin05 <georginthomas05@gmail.com>
2025-07-02 17:46:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
-<?php 
-$servername="localhost";
-$database="WMS";
-$username="root";
-$password="";
+<?php
+// db_connect.php
+$host     = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'WMS';
 
-$conn=new mysqli($servername,$database,$database,$password);
-?>
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die('Database connection failed: ' . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4'); // optional, but recommended
